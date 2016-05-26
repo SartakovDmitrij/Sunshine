5422a8612bcf3e3084dbb9526aed4ee8c2ce496b
SartakovDmitrij <dmitrij.sartakov@privatbank.ua>
2016-05-26 20:13:24 +0300

    shared action is added

--- app/src/main/java/com/udacity/android/sunshine/MainActivity.java
+++ app/src/main/java/com/udacity/android/sunshine/MainActivity.java
@@ -1,16 +1,15 @@
 package com.udacity.android.sunshine;
-
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends ActionBarActivity  {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
